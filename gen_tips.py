ace8f420934213f130f9cdd30cf84dc51ed5684a
Chetan Murthy <chetancheetah@gmail.com>
2018-03-22 17:43:37 +0000

    fixed more formatting

--- gen_tips.py
+++ gen_tips.py
@@ -1,8 +1,17 @@
 import sys
 import csv
 from datetime import datetime
+import operator
 
-shift = {}
+shift = {'House' : [
+    {'Name' : 'House',
+     'Staff Type' : 'Owner',
+     'Clock-In' : '2000-01-01 00:00',
+     'Clock-Out': '2100-01-01 00:00',
+     'Duration' : '0.0',
+     'Hourly Rate' : '0.0',
+     'Pay' : '0.0',
+ } ]}
 trans = []
 
 with open(sys.argv[1]) as f:
@@ -24,10 +33,11 @@ for row in rows:
         cols1[keys[i]] = col
         i += 1
     if s:
-        if shift.has_key(cols[0]):
-            shift[cols[0]].append(cols1)
+        if cols1['Staff Type'] == 'Owner': continue
+        if shift.has_key(cols1['Name']):
+            shift[cols1['Name']].append(cols1)
         else:
-            shift[cols[0]] = [cols1]
+            shift[cols1['Name']] = [cols1]
     else:
         trans.append(cols1)
 
@@ -40,8 +50,8 @@ shared_tips= {
     'Bartender': 0.05,
 }
 
-report = {'Admin':{'hours':0.0, 'pay':0.0, 'tips':0.0, 'cash':0.0}}
-for u in shift.keys(): report[u] = {'hours':0.0, 'pay': 0.0, 'tips':0.0, 'cash':0.0}
+report = {'Admin':{'type':'House', 'hours':0.0, 'pay':0.0, 'tips':0.0, 'cash':0.0}}
+for u in shift.keys(): report[u] = {'type':shift[u][0]['Staff Type'],'hours':0.0, 'pay': 0.0, 'tips':0.0, 'cash':0.0}
 
 for name, shifts in shift.iteritems():
     for s in shifts:
@@ -49,7 +59,7 @@ for name, shifts in shift.iteritems():
         report[name]['pay'] += float(s['Pay'][1:])
 # go over all the shift details
 for t in trans:
-    #report[t['Staff']]['tips'] += float(t['Tip'][1:])*0.7 + float(t['Gratuity'][1:])*0.7
+    report[t['Staff']]['tips'] += float(t['Tip'][1:])*0.7 + float(t['Gratuity'][1:])*0.7
     if t['Name'] == 'Cash':
         report[t['Staff']]['cash'] += float(t['Payment Amount'][1:])
 
@@ -66,7 +76,7 @@ for t in trans:
                 if  fr <= tran and tran <= to:
                     worked += 1
         if worked == 0:
-            report['Yogesh']['tips'] += float(t['Tip'][1:])*shared_tips[staff] + float(t['Gratuity'][1:])*shared_tips[staff]
+            report['House']['tips'] += float(t['Tip'][1:])*shared_tips[staff] + float(t['Gratuity'][1:])*shared_tips[staff]
 #            print "No one worked as " + staff + " for bill " + t['Bill Number'] + " at " + t['Bill Date']
         for name, val in shift.iteritems():
             if val[0]['Staff Type'] != staff: continue
@@ -79,13 +89,14 @@ for t in trans:
                     report[name]['tips'] += (float(t['Tip'][1:])*shared_tips[staff] + float(t['Gratuity'][1:])*shared_tips[staff]) / worked
                 
 
-print "{:<30} {:<15} {:<15} {:<10}  {:<10}  {:<10} ".format('Name','Hours','Pay', 'tips','cash-advance', 'Total')
+
+print "{:<30} {:<15} {:<10} {:<10} {:<10}  {:<10}  {:<10} ".format('Name','Type', 'Hours','Pay', 'tips','cash-advance', 'Total')
 hours, pay, tips, cash = 0.0, 0.0, 0.0, 0.0
-for k, v in report.iteritems():
-    print "{:<30} {:<15} {:<15} {:<10}  {:<10}  {:<10}".format(k, v['hours'], v['pay'], v['tips'], v['cash'], v['pay'] + v['tips']- v['cash'])
+for k, v in sorted(report.items()):
+    print "{:<30} {:<15} {:<10} {:<10} {:<10}  {:<10}  {:<10} ".format(k, v['type'], v['hours'], v['pay'], v['tips'], v['cash'], v['pay'] + v['tips']- v['cash'])
     hours += v['hours']
     pay += v['pay']
     tips += v['tips']
     cash += v['cash']
     
-print "{:<30} {:<15} {:<15} {:<10}  {:<10}  {:<10}".format("Total", hours, pay, tips, cash, hours+pay+tips+cash)
+print "{:<30} {:<15} {:<10} {:<10} {:<10}  {:<10}  {:<10} ".format("Total", "", hours, pay, tips, cash, hours+pay+tips-cash)
