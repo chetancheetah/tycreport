afdebb4f2cb180744a571d2bffe8ec9ca45b7792
Chetan Murthy <cmurthy@aristotle.corp.innovium.com>
2018-08-16 16:40:28 -0700

    changes

--- analyze_items.py
+++ analyze_items.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 input_dir = sys.argv[1]
 
-report = {}
+report={}
 weeks = {}
 for root, _, filenames in os.walk(input_dir):
     for filename in fnmatch.filter(filenames, "*.csv"):
@@ -17,6 +17,7 @@ for root, _, filenames in os.walk(input_dir):
             if 'theyellow' in row:
                 date = row.split('-')
                 week="%s/%s - %s/%s"%(date[3], date[4], date[6], date[7])
+                week = week[:-1]
                 weeks[week] = 1
                 continue
             if 'Menu Item' in row: continue
@@ -37,14 +38,25 @@ for root, _, filenames in os.walk(input_dir):
             cols = row.split(',')
             if cols[0] not in report.keys():
                 report[cols[0]] = {}
-            report[cols[0]][week] = float(cols[3]) + float(cols[5])
+                for c in ['Coolers', 'Mocktails', 'Beverages']:
+                    if cols[2] == c:
+                        cols[1] = 'Beverage'
+                        break
+                report[cols[0]]['cat'] = cols[1]
+            #report[cols[0]][week] = float(cols[3]) + float(cols[5])
+            report[cols[0]][week] = float(cols[5])
 
-for i in report.keys():
-    s = i + ","
-    for w in weeks:
-        if w not in report[i].keys():
-            s += ','
-        else:
-            s += str(report[i][w]) + ','
+for c in ['Food', 'Alcohol', 'Beverage']:
+    s = "Items,"
+    for w in weeks: s+= w+","
     print s
+    for i in report.keys():
+        if report[i]['cat'] != c:continue
+        s = i + ","
+        for w in weeks:
+            if w not in report[i].keys():
+                s += ','
+            else:
+                s += str(report[i][w]) + ','
+        print s
 
